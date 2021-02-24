1e43fe10fb06879485f59776ccf25d17ce89abb1
BrunoSaibert <brunosaibert@gmail.com>
2021-02-24 11:23:40 -0300

    feat: finalizar componente de Countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -8,6 +8,7 @@ const maxMinutes = 0.1 * 60;
 export function Countdown() {
   const [time, setTime] = useState(maxMinutes);
   const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
 
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
@@ -30,6 +31,9 @@ export function Countdown() {
       countdownTimeout = setTimeout(() => {
         setTime(time - 1);
       }, 1000);
+    } else if (isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
     }
   }, [isActive, time]);
 
@@ -47,22 +51,33 @@ export function Countdown() {
         </div>
       </div>
 
-      {isActive ? (
-        <button
-          type="button"
-          className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
-          onClick={resetCountdown}
-        >
-          Abandonar ciclo
+      {hasFinished ? (
+        <button disabled className={styles.countdownButton}>
+          Ciclo encerrado
+          <img src="icons/check.svg" alt="Ciclo encerrado" />
         </button>
       ) : (
-        <button
-          type="button"
-          className={styles.countdownButton}
-          onClick={startCountdown}
-        >
-          Iniciar um ciclo
-        </button>
+        <>
+          {isActive ? (
+            <button
+              type="button"
+              className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+              onClick={resetCountdown}
+            >
+              Abandonar ciclo
+              <img src="icons/close.svg" alt="Abandonar ciclo" />
+            </button>
+          ) : (
+            <button
+              type="button"
+              className={styles.countdownButton}
+              onClick={startCountdown}
+            >
+              Iniciar um ciclo
+              <img src="icons/start.svg" alt="Iniciar um ciclo" />
+            </button>
+          )}
+        </>
       )}
     </>
   );
