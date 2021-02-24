2104d8cb8cd481025de792d1165dc39b0f99dd3c
BrunoSaibert <brunosaibert@gmail.com>
2021-02-23 23:15:48 -0300

    feat: componente Profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import styles from "../styles/components/Profile.module.css";
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img
+        src="https://github.com/BrunoSaibert.png"
+        alt="Bruno Henrique Saibert"
+      />
+
+      <div>
+        <strong>Bruno Henrique Saibert</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
+}
