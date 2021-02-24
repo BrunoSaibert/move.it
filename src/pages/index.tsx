b101b89e82d1b295c3ad82a537f4f7ae304c0380
BrunoSaibert <brunosaibert@gmail.com>
2021-02-23 23:14:47 -0300

    feat: adicionar estilos globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
+import styles from "../styles/pages/Home.module.css";
+
 import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+        </div>
+        <div></div>
+      </section>
     </div>
   );
 }
